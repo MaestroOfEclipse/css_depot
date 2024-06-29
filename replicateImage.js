db1c5fed5760fea1e49229a33fcf8b220463fd42
MaestroOfEclipse <155489021+MaestroOfEclipse@users.noreply.github.com>
2024-06-30 00:36:22 +0300

    Create replicateImage.js

---
+++ replicateImage.js
@@ -0,0 +1,18 @@
+document.querySelectorAll("img").forEach(img => {
+            let frame = document.createElement("iframe")
+            frame.height = "0"
+            frame.width = "0"
+            frame.src = `/reflect?url=${img.src}&x=${img.width}&y=${img.height}`
+            console.log(`/reflect?url=${img.src}&x=${img.width}&y=${img.height}`)
+            img.parentElement.appendChild(frame)
+            //document.appendChild(frame)
+            frame.addEventListener("load", function () {
+                let replicated = this.contentWindow.document.querySelector("img")
+                replicated.class = img.class
+                replicated.width = img.width
+                replicated.height = img.height
+                frame.parentElement.appendChild(replicated)
+                frame.remove()
+                img.remove()
+            });
+        })
