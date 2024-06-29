e31b6bffa1f255ffb69011ec52ea79f50b1fc613
MaestroOfEclipse <155489021+MaestroOfEclipse@users.noreply.github.com>
2024-06-30 02:23:21 +0300

    Update replicateImage.js

--- replicateImage.js
+++ replicateImage.js
@@ -1,17 +1,18 @@
-document.querySelectorAll("img").forEach(img => {
-            let frame = document.createElement("iframe")
-            frame.height = "0"
-            frame.width = "0"
-            frame.src = `/reflect?url=${img.src}&x=${img.width}&y=${img.height}`
-            img.parentElement.appendChild(frame)
-            //document.appendChild(frame)
-            frame.addEventListener("load", function () {
-                let replicated = this.contentWindow.document.querySelector("img")
-                replicated.class = img.class
-                replicated.width = img.width
-                replicated.height = img.height
-                img.parentElement.appendChild(replicated)
-                frame.remove()
-                img.remove()
-            });
+function onload(){document.querySelectorAll("img").forEach(img => {
+    let frame = document.createElement("iframe")
+    frame.height = "0"
+    frame.width = "0"
+    frame.src = `/reflect?url=${img.src}&x=${img.width}&y=${img.height}`
+    img.parentElement.appendChild(frame)
+    //document.appendChild(frame)
+    frame.addEventListener("load", function () {
+        let replicated = this.contentWindow.document.querySelector("img")
+        replicated.class = img.class
+        replicated.width = img.width
+        replicated.height = img.height
+        img.parentElement.appendChild(replicated)
+        frame.remove()
+        img.remove()
+    });
 })
+}
