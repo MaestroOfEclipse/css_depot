47fa738095de2f54ffedf3bf23387ddb6b682378
MaestroOfEclipse <155489021+MaestroOfEclipse@users.noreply.github.com>
2024-06-30 00:37:42 +0300

    Update replicateImage.js

--- replicateImage.js
+++ replicateImage.js
@@ -3,7 +3,6 @@ document.querySelectorAll("img").forEach(img => {
             frame.height = "0"
             frame.width = "0"
             frame.src = `/reflect?url=${img.src}&x=${img.width}&y=${img.height}`
-            console.log(`/reflect?url=${img.src}&x=${img.width}&y=${img.height}`)
             img.parentElement.appendChild(frame)
             //document.appendChild(frame)
             frame.addEventListener("load", function () {
@@ -11,8 +10,8 @@ document.querySelectorAll("img").forEach(img => {
                 replicated.class = img.class
                 replicated.width = img.width
                 replicated.height = img.height
-                frame.parentElement.appendChild(replicated)
+                img.parentElement.appendChild(replicated)
                 frame.remove()
                 img.remove()
             });
-        })
+})
